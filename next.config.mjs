4870c3df49d03e2f2a6299aca9d3f3e01e31dadf
Merab Samkharadze <samkharadzemerab@gmail.com>
2026-04-17 13:58:12 +0400

    chore: disable eslint rule for Facebook Pixel noscript image tag

--- next.config.mjs
+++ next.config.mjs
@@ -24,11 +24,12 @@ const nextConfig = {
   async headers() {
     const csp = [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-      "img-src 'self' data: blob: https://*.supabase.co https://images.unsplash.com",
+      "img-src 'self' data: blob: https://*.supabase.co https://images.unsplash.com https://www.facebook.com https://tr.facebook.com",
       "font-src 'self' data: https://fonts.gstatic.com",
-      "connect-src 'self' https://*.supabase.co https://generativelanguage.googleapis.com",
+      "connect-src 'self' https://*.supabase.co https://generativelanguage.googleapis.com https://www.facebook.com https://connect.facebook.net",
+      "frame-src https://www.facebook.com",
       "frame-ancestors 'self'",
       "base-uri 'self'",
       "form-action 'self'",
